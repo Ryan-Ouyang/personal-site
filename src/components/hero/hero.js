ff67de36d111a83427be219a8cb832beef283926
Ryan Ouyang <ryan.ouyang01@gmail.com>
2019-10-22 22:31:50 -0400

    Added media queries

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,32 +1,49 @@
 import React from "react"
 import styled from "styled-components"
+import {device} from "../../utils/media-queries"
 
 const HeroSection = styled.section`
+    box-sizing: border-box;
     height: 100vh;
-    max-height: 100vh;
     display: flex;
     flex-direction: column;
-    overflow: hidden;
-    width: 100vw;
-    margin: 0 auto
-    padding: 150px 0px 150px 100px
+    justify-content: space-around;
+    max-width: 1440px;
+    margin: 0 auto;
     background-color: #fffff4
+
+    padding: 150px 100px;
+    @media ${device.desktop} { padding: 150px 100px }
+    @media ${device.laptop} { padding: 150px 100px }
+    @media ${device.tablet} { padding: 100px 70px }
+    @media ${device.mobile} { padding: 70px 50px }
 `
 
 const Hey = styled.h1`
     font-size: 2.5rem;
     font-weight: 300;
+
+    @media ${device.desktop} { font-size: 2.5rem; }
+    @media ${device.laptop} { font-size: 2.25rem; }
+    @media ${device.tablet} { font-size: 2.00rem; }
+    @media ${device.mobile} { font-size: 1.75rem; }
 `
 
 const Tagline = styled.h1`
     font-size: 2.25rem;
     font-weight: 300;
     max-width: 750px;
+
+    @media ${device.desktop} { font-size: 2.5rem; max-width: 750px; }
+    @media ${device.laptop} { font-size: 2.25rem; max-width: 600px; }
+    @media ${device.tablet} { font-size: 2.00rem; max-width: 500px; }
+    @media ${device.mobile} { font-size: 1.75rem; max-width: 325px; }
 `
 
 const ContactMe = styled.h1`
     font-size: 1.25rem;
     font-weight: 400;
+
 `
 
 export default class Hero extends React.Component {
