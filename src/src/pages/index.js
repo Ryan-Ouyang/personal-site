5b0e4ad25c120eecc340ae10edadda9645dd37c9
Ryan Ouyang <ryan.ouyang01@gmail.com>
2019-10-22 21:08:26 -0400

    Initial commit

---
+++ src/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Hero from '../components/hero/hero'
+
+export default class App extends React.Component {
+    render() {
+        return(
+            <Hero />
+        )
+    }
+}
