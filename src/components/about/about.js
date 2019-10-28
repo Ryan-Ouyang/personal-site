79f5f3beec23a2a05fbad74368be441c350abdee
Ryan Ouyang <ryan.ouyang01@gmail.com>
2019-10-28 15:28:35 -0400

    Added two column layout to about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react"
+import styled from "styled-components"
 
-import { device } from '../../utils/media-queries';
+import { device } from "../../utils/media-queries"
+
+const AboutSection = styled.section`
+    box-sizing: border-box;
+    max-width: 1440px;
+    margin: 0 auto;
+
+    padding: 20px 100px;
+    @media ${device.desktop} {
+        padding: 20px 100px;
+    }
+    @media ${device.laptop} {
+        padding: 20px 100px;
+    }
+    @media ${device.tablet} {
+        padding: 20px 70px;
+    }
+    @media ${device.mobile} {
+        padding: 20px 50px;
+    }
+`
+
+const TwoColumns = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    @media ${device.tablet} {
+        flex-direction: column;
+    }
+`
+
+const Column = styled.div``
+
+const LeftColumn = styled(Column)`
+    flex-grow: 3;
+    background-color: #ddd;
+`
+
+const RightColumn = styled(Column)`
+    flex-grow: 8;
+    background-color: #eee;
+`
 
 export default class About extends React.Component {
-    render () {
-        return(
-            <h1>Template</h1>
+    render() {
+        return (
+            <AboutSection>
+                <TwoColumns>
+                    <LeftColumn>
+                        <h1>test</h1>
+                    </LeftColumn>
+                    <RightColumn>
+                        <h1>test</h1>
+                    </RightColumn>
+                </TwoColumns>
+            </AboutSection>
         )
     }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ export default class App extends React.Component {
             <>
                 <GlobalStyle />
                 <Hero />
-                {/* <About /> */}
+                <About />
             </>
         )
     }
