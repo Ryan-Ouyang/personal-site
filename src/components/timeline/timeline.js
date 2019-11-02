af5b2a40372cb1236aee6267f2c30eef81895de9
Ryan Ouyang <ryan.ouyang01@gmail.com>
2019-11-02 15:32:53 -0400

    Added responsive font size

---
+++ src/components/timeline/timeline.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { device } from '../../utils/media-queries';
+
+export default class About extends React.Component {
+    render () {
+        return(
+            <h1>Timeline here</h1>
+        )
+    }
+}
